82bed9c219612533e67913900abac925358e5d12
Vsevolod Stakhov <vsevolod.stakhov@parity.io>
2022-07-05 09:18:29 +0100

    Add more features and neats

--- src/kvdb/mod.rs
+++ src/kvdb/mod.rs
@@ -25,6 +25,17 @@ use strum::{Display, EnumString};
 
 pub use crate::kvdb::traits::*;
 
+/// Specific options for the usage subcommand
+#[derive(Clone, Debug, Parser)]
+#[clap(rename_all = "kebab-case")]
+pub(crate) struct KvdbUsageOpts {
+	/// Check only specific column(s)
+	#[clap(long, short = 'c')]
+	column: Vec<String>,
+	#[clap(long, short = 'p')]
+	keys_prefix: Vec<String>,
+}
+
 /// Mode of this command
 #[derive(Clone, Debug, Parser)]
 #[clap(rename_all = "kebab-case")]
@@ -32,7 +43,7 @@ pub(crate) enum KvdbMode {
 	/// Returns list of all columns in the database
 	Columns,
 	/// Returns usage in the database
-	Usage,
+	Usage(KvdbUsageOpts),
 }
 
 /// Mode of this command
@@ -51,43 +62,64 @@ impl Default for KvdbType {
 	}
 }
 
+/// Output mode for the CLI commands
+#[derive(Clone, Debug, Parser, EnumString, Display)]
+#[clap(rename_all = "kebab-case")]
+pub(crate) enum OutputMode {
+	#[strum(ascii_case_insensitive)]
+	Pretty,
+	#[strum(ascii_case_insensitive)]
+	Json,
+}
+
+impl Default for OutputMode {
+	fn default() -> Self {
+		OutputMode::Pretty
+	}
+}
+
 #[derive(Clone, Debug, Parser)]
 #[clap(rename_all = "kebab-case")]
 pub struct KvdbOptions {
 	/// Path to the database
 	#[clap(long)]
 	db: String,
-
 	#[clap(long, default_value_t)]
 	db_type: KvdbType,
 	/// Mode of running
 	#[clap(subcommand)]
 	mode: KvdbMode,
+	/// Output mode
+	#[clap(long, default_value_t)]
+	output: OutputMode,
 }
 
 #[derive(Clone, Debug, Serialize)]
-struct UsageResults {
+struct UsageResults<'a> {
+	description: &'a str,
 	keys_count: usize,
 	keys_size: usize,
 	values_size: usize,
 }
 
-impl Display for UsageResults {
+impl<'a> Display for UsageResults<'a> {
 	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
 		if self.keys_count > 0 {
 			write!(
 				f,
-				"{} keys size: {} bytes, values size: {} bytes ({:.2} bytes per key in average)",
+				"{}: {} keys size: {} bytes ({:.2} bytes per key in average), values size: {} bytes ({:.2} bytes per value in average)",
+				self.description,
 				self.keys_count,
 				self.keys_size,
+				self.keys_size as f64 / self.keys_count as f64,
 				self.values_size,
 				self.values_size as f64 / self.keys_count as f64
 			)
 		} else {
 			write!(
 				f,
-				"{} keys size: {} bytes, values size: {} bytes (0 bytes per key in average)",
-				self.keys_count, self.keys_size, self.values_size
+				"{}: {} keys size: {} bytes (0 bytes per key in average), values size: {} bytes (0 bytes per value in average)",
+				self.description, self.keys_count, self.keys_size, self.values_size
 			)
 		}
 	}
@@ -111,25 +143,54 @@ fn run_with_db<D: IntrospectorKvdb>(db: D, opts: KvdbOptions) -> Result<()> {
 				println!("{}", col.as_str());
 			}
 		},
-		KvdbMode::Usage => {
-			let columns = db.list_columns()?;
+		KvdbMode::Usage(ref usage_opts) => {
+			let columns = db.list_columns()?.iter().filter(|col| {
+				if !usage_opts.column.is_empty() {
+					usage_opts.column.contains(col)
+				} else {
+					true
+				}
+			});
 
 			for col in columns {
 				let mut keys_space = 0_usize;
 				let mut keys_count = 0_usize;
 				let mut values_space = 0_usize;
-				let iter = db.iter_values(col.as_str())?;
+				let iter = db.iter_values(col.as_str())?.filter(|(key, _)| {
+					if !usage_opts.keys_prefix.is_empty() {
+						usage_opts.keys_prefix.iter().any(|prefix| key.starts_with(prefix.as_bytes()))
+					} else {
+						true
+					}
+				});
 				for (key, value) in iter {
 					keys_space += key.len();
 					keys_count += 1;
 					values_space += value.len();
 				}
 
-				let res = UsageResults { keys_count, values_size: values_space, keys_size: keys_space };
+				let res = UsageResults {
+					description: col.as_str(),
+					keys_count,
+					values_size: values_space,
+					keys_size: keys_space,
+				};
 
-				println!("{}: {}", col.as_str(), res);
+				output_result(&res, &opts)?;
 			}
 		},
+	}
+
+	Ok(())
+}
+
+fn output_result<T>(res: &T, opts: &KvdbOptions) -> Result<()>
+where
+	T: Display + Serialize,
+{
+	match opts.output {
+		OutputMode::Json => println!("{}", serde_json::to_string(res)?),
+		OutputMode::Pretty => println!("{}", res),
 	}
 
 	Ok(())
